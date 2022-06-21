7720f0666cbe67fd1215c2afb2827b0ba5b8eb7d
0xKnut <knut@0xcarbon.org>
2022-06-21 19:04:13 +0200

    remove snx stuff

--- plugins/index.js
+++ plugins/index.js
@@ -1,7 +1,6 @@
 const helpers = require('../support/helpers');
 const puppeteer = require('../commands/puppeteer');
 const metamask = require('../commands/metamask');
-const synthetix = require('../commands/synthetix');
 const etherscan = require('../commands/etherscan');
 
 /**
@@ -242,25 +241,6 @@ module.exports = (on, config) => {
         password,
       });
       return true;
-    },
-    snxExchangerSettle: async ({ asset, walletAddress, privateKey }) => {
-      if (process.env.PRIVATE_KEY) {
-        privateKey = process.env.PRIVATE_KEY;
-      }
-      const settled = await synthetix.settle({
-        asset,
-        walletAddress,
-        privateKey,
-      });
-      // todo: wait for confirmation?
-      return settled;
-    },
-    snxCheckWaitingPeriod: async ({ asset, walletAddress }) => {
-      const waitingPeriod = await synthetix.checkWaitingPeriod({
-        asset,
-        walletAddress,
-      });
-      return waitingPeriod;
     },
     getNetwork: () => {
       const network = helpers.getNetwork();
